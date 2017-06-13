aeae54f742f8f525b03d8d8e3513fd385a2e9760
Me <Me>
2017-06-12 20:08:22 -0700

    Added TODO for customized swagger documentation

--- demo/src/main/java/com/singh/config/Swagger.java
+++ demo/src/main/java/com/singh/config/Swagger.java
@@ -10,6 +10,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 /*
  * Basic: http://localhost:8080/swagger-ui.html
+ * TODO: Make url custom
  */
 @Configuration
 @EnableSwagger2
