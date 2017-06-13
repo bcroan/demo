9ecaf2f52b7e666cd22b2f5c38dd429a321322aa
Owner <Owner@LAPTOP-FVDIG07P>
2017-06-12 20:03:19 -0700

    Basic swagger url documented in comments

--- demo/src/main/java/com/singh/config/Swagger.java
+++ demo/src/main/java/com/singh/config/Swagger.java
@@ -8,7 +8,9 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+/*
+ * Basic: http://localhost:8080/swagger-ui.html
+ */
 @Configuration
 @EnableSwagger2
 public class Swagger {
